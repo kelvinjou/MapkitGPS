7ddaaf85744dc9a12854f79ef3629b04aa4042be
kelvinjou <63611619+kelvinjou@users.noreply.github.com>
2021-04-01 22:25:41 -0700

    Initial Commit

---
+++ GPSExample/GPSExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSExampleApp.swift
+//  GPSExample
+//
+//  Created by mimi on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct GPSExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
